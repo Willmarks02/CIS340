a79a6d8ca506603b804522efb482b939fc27db74
Willmarks02 <willm02@colostate.edu>
2024-04-21 20:56:56 -0600

    MyFourthProject

--- MyFourthProject/App.js
+++ MyFourthProject/App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  React from 'react';
+import { Text, Image, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function MyScrollViewApp(){
+
+  const dog = {
+    uri: 'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340-Images/main/images/mydog.jpg',
+    width: 64,
+    height: 64
+  }
+
+  return(
+
+        <ScrollView style={{padding: 40}}>
+          <Text style={{fontSize: 80}}> Try to scroll down</Text>
+          <Image source={require('./assets/dog2.png')}style={{width: 80, height: 80}} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Text style={{fontSize: 80}}> Try to scroll down again, if you like</Text>
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Text style={{fontSize: 80}}> Try to scroll down</Text>
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Text style={{fontSize: 80}}> Try to scroll down</Text>
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Text style={{fontSize: 80}}> React Native Components!</Text>
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+          <Image source={dog} />
+        </ScrollView>
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
