5eb82c7fb52fa18ce3c5e6559952e1abefc42a99
Willmarks02 <willm02@colostate.edu>
2024-05-01 11:15:15 -0600

    Adding Buttons and Upload Images

---
+++ MyEighthProject/App.js
@@ -0,0 +1,115 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker' // import picker
+import * as Sharing from 'expo-sharing'; //import image sharing
+
+export default function App() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null)
+
+  let openImagePickerAsync = async () => {
+
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+  
+    if (permissionResult.granted === false){
+  
+      alert("Permission is Required!")
+      return;
+    }
+  
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+   
+    if (pickerResult.cancelled === true){
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri});
+  
+  }
+
+  let openSharingDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())){
+      alert('Sharing is not avaliable on my phone')
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.localUri);
+  }
+
+
+//display the slected image
+  if (selectedImage !== null){
+    return(
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri}} style={styles.selImage}/>
+
+        <TouchableOpacity onPress={openSharingDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}> Share My Phone</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: 'https://github.com/AbdunabiRamadan/CIS340-Images/blob/main/images/logo.png'}}
+              style={styles.logo}/>
+      <Text style={styles.insts}>
+        Press the button below to select an image on your phone!
+      </Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => alert('You have not selected an image yet')}>
+
+        <Text style={styles.buttonText}>Pick Image</Text>
+
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#FFFFE0",
+  },
+
+  button: {
+
+    backgroundColor: "#778899",
+    padding: 20,
+    borderRadius: 5
+
+  },
+
+  insts: {
+    fontSize: 18,
+    color: "87CEFA",
+    marginHorizontal: 15,
+    marginBottom: 10,
+
+  },
+
+  logo: {
+    width: 310,
+    height: 300,
+    marginBottom: 20
+
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: "#fff"
+
+  },
+
+  selImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
+
+  }
+
+
+});
